4a1200545caa6d8be5fffbcc5f6745efd37aea8c
clydedz <clyd94@gmail.com>
2017-10-21 23:44:19 +1300

    Added header meta data along with other partial views.

--- WebApp/WebApp/Scripts/app.js
+++ WebApp/WebApp/Scripts/app.js
@@ -27,68 +27,41 @@
     }
 }
 
-function Slider(element) {
-    this.el = document.querySelector(element);
-    this.init();
+var slideIndex = 0;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-Slider.prototype = {
-    init: function () {
-        this.links = this.el.querySelectorAll("#slider-nav a");
-        this.wrapper = this.el.querySelector("#slider-wrapper");
-        this.navigate();
-    },
-    navigate: function () {
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-        for (var i = 0; i < this.links.length; ++i) {
-            var link = this.links[i];
-            this.slide(link);
-        }
-    },
-
-    animate: function (slide) {
-        var parent = slide.parentNode;
-        //var caption = slide.querySelector(".caption");
-        //var captions = parent.querySelectorAll(".caption");
-        //for (var k = 0; k < captions.length; ++k) {
-        //    var cap = captions[k];
-        //    if (cap !== caption) {
-        //        cap.classList.remove("visible");
-        //    }
-        //}
-        //caption.classList.add("visible");
-    },
-
-    slide: function (element) {
-        var self = this;
-        element.addEventListener("click", function (e) {
-            e.preventDefault();
-            var a = this;
-            self.setCurrentLink(a);
-            var index = parseInt(a.getAttribute("data-slide"), 10) + 1;
-            var currentSlide = self.el.querySelector(".slide:nth-child(" + index + ")");
-
-            self.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
-            //self.animate(currentSlide);
-
-        }, false);
-    },
-    setCurrentLink: function (link) {
-        var parent = link.parentNode;
-        var a = parent.querySelectorAll("a");
-
-        link.className = "current";
-
-        for (var j = 0; j < a.length; ++j) {
-            var cur = a[j];
-            if (cur !== link) {
-                cur.className = "";
-            }
-        }
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-};
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-    var aSlider = new Slider("#slider");
-
-});
+currentSlide(1);
+//function showSlides() {
+//    var i;
+//    var slides = document.getElementsByClassName("mySlides");
+//    for (i = 0; i < slides.length; i++) {
+//        slides[i].style.display = "none";
+//    }
+//    slideIndex++;
+//    if (slideIndex > slides.length) { slideIndex = 1 }
+//    slides[slideIndex - 1].style.display = "block";
+//    setTimeout(showSlides, 80000); // Change image every 2 seconds
+//}
